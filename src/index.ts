47c4090c3fd772abdc622a3dbc1802b8f3dbc8d8
Angelo Evangelista <angeloevan.ane@gmail.com>
2025-02-17 22:06:37 -0300

    fix: try to get the correct name by removin parentheses
    I am doing that 'cause it failed to fetch an specific song I was really interested on getting the lyrics :/

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,46 @@ app.use(cors());
 app.get("/api/health", (request, response) => response.json({ ok: true }));
 
 app.get("/api/lyrics", async (request, response) => {
-  const { artist, song } = request.query;
+  const { artist = "", song = "" } = request.query as {
+    [key: string]: string | undefined;
+  };
 
-  try {
-    const lyricsResponse = await axios.get("https://api.textyl.co/api/lyrics", {
+  if (!artist || !song) {
+    return response.status(400).json({
+      error:
+        "The artist and song are query params. But what the hell u expect me to do without them???",
+    });
+  }
+
+  const possibilities = [
+    {
+      artist,
+      song,
+    },
+    {
+      artist:
+        artist.slice(
+          0,
+          artist.indexOf("(") != 1 ? artist.indexOf("(") - 1 : 0
+        ) ?? "",
+      song:
+        song.slice(0, song.indexOf("(") != 1 ? song.indexOf("(") - 1 : 0) ?? "",
+    },
+  ];
+
+  const requests = possibilities.map((p) =>
+    axios.get("https://api.textyl.co/api/lyrics", {
       params: {
-        q: `${artist} ${song}`,
+        q: `${p.artist} ${p.song}`,
       },
       httpsAgent: new https.Agent({
         rejectUnauthorized: false,
       }),
-    });
+    })
+  );
+
+  try {
+    const lyricsResponse = await Promise.any(requests);
 
     return response.json(lyricsResponse.data);
   } catch (error: any) {
